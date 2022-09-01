22466a9cf3a212cf2cb61c73d5fbdf278b6f9d9c
AthertonH <athertonharrison@gmail.com>
2022-08-31 22:34:50 -0700

    python

--- member_questions/main.py
+++ member_questions/main.py
@@ -1,17 +1,14 @@
-# script to assign questions to my Math 181 (Calc 1) group
-
 def member_questions(problems):
     members = ["Harrison", "Cable", "Daniely", "Joshua", "Izaiah", "Nathan", "Destiny"]
-    m_index = 0
+    m_index = 3
     for p in range(1, problems):
-       f p % 2 == 0:
+        if p % 2 == 0:
             print(p, end=": ")
-            if m_index/7 != 1:
+            if m_index != 6:
                 print(members[m_index])
                 m_index += 1
             else:
-                m_index = 0
                 print(members[m_index])
+                m_index = 0
         else:
             continue
-
